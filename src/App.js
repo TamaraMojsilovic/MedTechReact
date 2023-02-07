6b7195bb041fd3fad92c55c82fa42d07af037975
TamaraMojsilovic <mojsilovictamara324@gmail.com>
2023-02-07 14:50:44 +0100

    kreiran niz pregleda

--- src/App.js
+++ src/App.js
@@ -4,7 +4,49 @@ import Navbar from './komponente/Navbar';
 import WelcomePage from './komponente/WelcomePage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 function App() {
+
+
+
+
+  const [pregledi,setPregledi] = useState([
+    {
+      id:1,
+      naziv:"Pregled specijaliste",
+      cena:3500,
+      doktor:"Jovanovic Branka",
+      pacijent:"Budimir Ana",
+
+    },
+    {
+      id:2,
+      naziv:"IVF",
+      cena: 150000,
+      doktor:"Ana Markovic",
+      pacijent:"Pera Peric",
+
+    },
+    {
+      id:3,
+      naziv:"Ultrazvucni pregled",
+      cena:4000,
+      doktor:"Jovanovic Milos",
+      pacijent:"Marko Petrovic",
+
+    },
+    {
+      id:4,
+      naziv:"Konsultacije bez ultrazvuka",
+      cena: 2400,
+      doktor:"Jovanovic Branka",
+      pacijent:"Budimir Ana",
+
+    },
+
+  ]);
+
+
   return (
     <div className="App">
       
