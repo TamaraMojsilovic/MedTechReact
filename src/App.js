9231a05fd13ec79a4aef162639650e768c0cd0e8
TamaraMojsilovic <mojsilovictamara324@gmail.com>
2023-02-07 14:45:49 +0100

    dodata reusable komponenta navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
  
 import './App.css';
+import Navbar from './komponente/Navbar';
 import WelcomePage from './komponente/WelcomePage';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       
-      <WelcomePage></WelcomePage>
+      
+      <BrowserRouter>
+      <Navbar  ></Navbar>
+      <Routes>
+        <Route path="/" element={ <WelcomePage></WelcomePage>}></Route>
+     
+
+
+      </Routes>
+    
+       
+
+      </BrowserRouter>
+     
     </div>
   );
 }
---
+++ src/komponente/Navbar.jsx
@@ -0,0 +1,19 @@
+ 
+ import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+     
+        <nav className="navbar navbar-dark bg-dark">
+        < Link  to="/" className="navbar-brand">Pocetna</Link>
+ 
+
+   
+        </nav>
+
+
+     
+  );
+}
+
+export default Navbar;
